ff397f237e9871e4c6f3955040f7ad81a8b4d0f8
bsig1 <blake.sigmundstad@gmail.com>
2025-04-07 21:25:26 -0600

    website

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { useState } from 'react'
 import bslogo from './assets/logo.svg'
 import './App.css'
-
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
 function App() {
 
   return (
@@ -12,6 +17,15 @@ function App() {
         <div id ="title">
             <h1>blakesite</h1>
         </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="contact" element={<Contact />} />
+                    <Route path="*" element={<NoPage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     </>
   )
 }
