a7c3e52a190c92c9fb53488715535cb6937ff5f5
bsig1 <blake.sigmundstad@gmail.com>
2025-10-13 20:50:09 -0600

    chess start

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,9 +4,11 @@ import "./Contact.css"
 const Contact = () => {
     return (
         <>
-            <h1 className="page-title">--Contact Me--</h1>
-            <h2>Blake Sigmundstad</h2>
-            <h2>blake.sigmundstad@gmail.com</h2>
+            <div id={"contact_div"}>
+                <h1 className="page-title">Contact Me</h1>
+                <h2>Blake Sigmundstad</h2>
+                <h2>blake.sigmundstad@gmail.com</h2>
+            </div>
         </>
     )
 };
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,25 +1,24 @@
-import "./Projects.css"
-import logo_2048 from"../assets/2048_logo.svg.png"
-import logo_chess from"../assets/chessicon.jpg"
-import {Link} from "react-router-dom";
+import "./Projects.css";
+import logo_2048 from "../assets/2048_logo.svg.png";
+import logo_chess from "../assets/chessicon.jpg";
+import { Link } from "react-router-dom";
 
 const Projects = () => {
     return (
         <>
-            <Link to="/projects/Project_2048">
-                <div className="project-tile">
-                    <img src={logo_2048} alt="2048"/>
-                </div>
-            </Link>
-            <Link to="/projects/Chess">
-                <div className="project-tile">
-                    <img src={logo_chess} alt="Chess"/>
-                </div>
-            </Link>
+            <h1 id="page-title" className="page-title">Projects</h1>
 
+            <div className="projects-grid">
+                <Link to="/projects/Project_2048" className="project-tile">
+                    <img src={logo_2048} alt="2048" />
+                </Link>
+
+                <Link to="/projects/Chess" className="project-tile">
+                    <img src={logo_chess} alt="Chess" />
+                </Link>
+            </div>
         </>
-    )
-
+    );
 };
 
 export default Projects;
